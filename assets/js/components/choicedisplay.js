ffb677c3095afd891335b33277e6ff4878d044d1
Daryl Ando <ando.daryl@hotmail.com>
2022-02-12 14:53:39 +0000

    Add choice display

---
+++ assets/js/components/choicedisplay.js
@@ -0,0 +1,48 @@
+// Module for points display component
+
+// Import dependencies
+import constantsJS from "../module/constants.js"
+import pointsJS from "../module/points.js"
+
+// Export IIFE
+export default (function() {
+  // Get exports
+  const {PLAYER, AI} = constantsJS
+  const {isGameOver} = pointsJS
+
+  const bindChoiceDisplayBehaviour = (choiceContainer) => (playerChoice, aiChoice) => {
+    
+      // Main behaviour
+      const showChoice = (owner, choice) => {
+      // Get element nodes
+      const ownerAttribute = `[data-choice="${owner}"]`
+      const playerChoiceContainer = choiceContainer.querySelector(ownerAttribute)
+      const rockNode = playerChoiceContainer.querySelector('[data-choice="ROCK"]')
+      const paperNode = playerChoiceContainer.querySelector('[data-choice="PAPER"]')
+      const scissorsNode = playerChoiceContainer.querySelector('[data-choice="SCISSORS"]')
+      const choiceAttribute = `[data-choice="${choice}"]`
+      const choiceNode = playerChoiceContainer.querySelector(choiceAttribute)
+
+      // Hide all nodes
+      rockNode.classList.add('visually-hidden')
+      paperNode.classList.add('visually-hidden')
+      scissorsNode.classList.add('visually-hidden')
+      
+      // Display chosen node
+      choiceNode.classList.remove('visually-hidden') 
+
+      console.log(owner)
+      console.log(choice)
+    }
+    // Activate behaviour
+    const gameIsNotOver = !isGameOver()
+    if (gameIsNotOver) {
+      showChoice(PLAYER, playerChoice)
+      showChoice(AI, aiChoice)
+    }
+  }
+  // API
+  return {
+    bindChoiceDisplayBehaviour
+  }
+})()
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,24 @@
 // Import dependencies
 import inputJS from "./components/input.js";
 import pointsdisplayJS from "./components/pointsdisplay.js";
+import choicedisplayJS from "./components/choicedisplay.js";
 
 // Get imports
 const {bindInputBehaviour} = inputJS
 const {bindPointsDisplayBehaviour} = pointsdisplayJS
+const {bindChoiceDisplayBehaviour} = choicedisplayJS 
+
 
 // DOM Elements
 const title = document.getElementsByTagName('title')[0]
 const node = document.getElementById('score-node')
 const inputContainer = document.getElementById('container-input')
+const choiceContainer = document.getElementById('container-choice')
 
 // Bind Behaviours to DOM Elements
 const updatePointsDisplay = bindPointsDisplayBehaviour(title, node)
+const updateChoiceDisplay = bindChoiceDisplayBehaviour(choiceContainer)
 const activateInputBehaviour = bindInputBehaviour(inputContainer)
 
 // Game Logic
-activateInputBehaviour(updatePointsDisplay)
+activateInputBehaviour(updatePointsDisplay, updateChoiceDisplay)
