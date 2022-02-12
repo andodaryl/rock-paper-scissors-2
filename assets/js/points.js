938b9edac326244d378c669d26e9512e4235c149
Daryl Ando <ando.daryl@hotmail.com>
2022-02-12 10:43:17 +0000

    Add error checking via classes + read only data + commentary for structure

--- assets/js/points.js
+++ assets/js/points.js
@@ -6,13 +6,53 @@ import {DEV_MODE, VERSUS, WIN, LOSE} from "./constants"
 // Export IIFE
 export default (function() {
 
-  const Data = {
-    AI: 0,
-    PLAYER: 0,
+  // Data Integrity Check
+  const isPoints = input => !!parseInt(input) // Check if int & convert truthy to boolean
+
+  // Classes
+  class Data {
+    #_Points = {
+      AI: 0,
+      PLAYER: 0
+    }
+    constructor(pointsObj) {
+      // Initial input
+      const safeObj = pointsObj || {}
+      const AI = safeObj.AI
+      const PLAYER = safeObj.PLAYER
+      // Sanitize input i.e. use default if unsafe
+      const SAFE_AI = isPoints(AI) ? AI : this.#_Points.AI 
+      const SAFE_PLAYER = isPoints(PLAYER) ? PLAYER : this.#_Points.PLAYER 
+      // Use safe inputs
+      this.AI = SAFE_AI
+      this.PLAYER = SAFE_PLAYER
+    }
+    // Getters & setters with error checking
+    get AI() {
+      return this.#_Points.AI
+    }
+    set AI(input) {
+      // Only update if safe input
+      if (isPoints(input)) this.#_Points.AI = input
+    }
+    get PLAYER(){
+      return this.#_Points.PLAYER
+    }
+    set PLAYER(input) {
+      // Only update if safe input
+      if (isPoints(input)) this.#_Points.PLAYER = input
+    }
   }
 
-  const playerWin = () => ++Data.PLAYER // Increase player points
-  const playerLose = () => ++Data.AI // Increase ai points
+  // Data
+  const currentPoints = new Data()
+
+  // Points logic
+  const playerWin = () => ++currentPoints.PLAYER // Increase player points
+  const playerLose = () => ++currentPoints.AI // Increase ai points
+
+  // Read only data
+  const getCurrentPoints = () => ({...currentPoints})
 
   // Check if player choice is sucessful
   const isWinningChoice = input => {
@@ -31,6 +71,7 @@ export default (function() {
   return {
     playerWin,
     playerLose,
-    isWinningChoice
+    isWinningChoice,
+    getCurrentPoints,
   }
 })()
