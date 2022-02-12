32eea5e31591e225aa9d55437c53a2eac026a40b
Daryl Ando <ando.daryl@hotmail.com>
2022-02-12 11:25:58 +0000

    Add max points behaviour

--- assets/js/components/pointsdisplay.js
+++ assets/js/components/pointsdisplay.js
@@ -1,6 +1,7 @@
 // Module for points display component
 
 // Import dependencies
+import {MAX_WIN} from "../module/constants"
 
 // Export IIFE
 export default (function() {
@@ -10,8 +11,18 @@ export default (function() {
     const PLAYER = safePoints.PLAYER || 0
     const AI = safePoints.AI || 0
     // Display logic
-    title.innerText = `${PLAYER}:${AI} - Rock Paper Scissors` // Update Title
-    node.innerText = `${PLAYER}:${AI}` // Update Score Node
+    if (PLAYER >= MAX_WIN) {
+      // Celebrate player win
+      title.innerText = `YOU WIN - Rock Paper Scissors`
+      node.innerText = `YOU WIN`
+    } else if (PLAYER >= MAX_WIN) {
+      // Celebrate ai win
+      title.innerText = `AI WINS - Rock Paper Scissors`
+      node.innerText = `AI WINS`
+    } else {
+      title.innerText = `${PLAYER}:${AI} - Rock Paper Scissors` // Update Title
+      node.innerText = `${PLAYER}:${AI}` // Update Score Node
+    }
   }
   // API
   return {
