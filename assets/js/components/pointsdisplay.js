21298b6c9ddd6ac0037e45e7df8906d83c821381
Daryl Ando <ando.daryl@hotmail.com>
2022-02-12 11:06:08 +0000

    Rename display to pointsdisplay & move to components folder & update var name

---
+++ assets/js/components/pointsdisplay.js
@@ -0,0 +1,20 @@
+// Module for points display component
+
+// Import dependencies
+
+// Export IIFE
+export default (function() {
+  const updatePointsDisplay = (title, node) => (currentPoints) => {
+    // Safe data
+    const safePoints = currentPoints || {}
+    const PLAYER = safePoints.PLAYER || 0
+    const AI = safePoints.AI || 0
+    // Display logic
+    title.innerText = `${PLAYER}:${AI} - Rock Paper Scissors` // Update Title
+    node.innerText = `${PLAYER}:${AI}` // Update Score Node
+  }
+  // API
+  return {
+    updatePointsDisplay
+  }
+})()
