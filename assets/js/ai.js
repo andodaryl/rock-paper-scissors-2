250a7a402fe024e4884ac1a8b941446f8344a068
Daryl Ando <ando.daryl@hotmail.com>
2022-02-12 10:09:23 +0000

    Update commentary

--- assets/js/ai.js
+++ assets/js/ai.js
@@ -3,12 +3,16 @@
 // Import dependencies
 import {ROCK, PAPER, SCISSORS} from "./constants"
 
+// Export IIFE
 export default (function() {
+  
   const getAiChoice = () => {
     const outcomes = [ROCK, PAPER, SCISSORS]
     const randomChoice = Math.floor(Math.random() * outcomes.length)
     return outcomes[randomChoice] 
   }
+
+// For use in other modules
   return {
     getAiChoice
   }
